1b2f26bd341d829adad019461a6444239ef75096
Killian0812 <93117684+Killian0812@users.noreply.github.com>
2024-06-11 10:08:02 +0700

    update recrawl

---
+++ DataCrawling/config.py
@@ -0,0 +1,8 @@
+import datetime
+
+api_key = "RGAPI-5b7d29c2-083f-4e2e-90d8-749b24639d6e"
+now = datetime.datetime.now()
+current_day = now.day
+current_month = now.month
+current_year = now.year
+current_date = f"{current_year}_{current_month}_{current_day}"
--- DataCrawling/puuid.py
+++ DataCrawling/puuid.py
@@ -1,27 +1,22 @@
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
 import requests
+from config import api_key, current_date
 
-import datetime
+print(f"puuid_{current_date}")
 
-# Lấy thời gian hiện tại
-now = datetime.datetime.now()
-current_day = now.day
-current_month = now.month
-current_year = now.year
-print(f"puuid_{current_year}_{current_month}_{current_day}")
 # Tạo Kafka consumer
-api_key = "RGAPI-c411c885-50e8-4cd2-84b6-a696859edc24"
 consumer = KafkaConsumer(
-    f"riot_id_{current_year}_{current_month}_{current_day}",  # Tên topic
+    f"riot_id_{current_date}",  # Tên topic
     bootstrap_servers='localhost:9092',  # Địa chỉ của Kafka broker
     enable_auto_commit=True,
     auto_commit_interval_ms=300,
     auto_offset_reset='earliest',  # Bắt đầu đọc từ đầu nếu không tìm thấy offset
-    group_id=f'puuid_{current_year}_{current_month}_{current_day}'  # ID nhóm cho consumer
+    group_id=f'puuid_{current_date}'  # ID nhóm cho consumer
 )
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
 # Xử lý mỗi message từ consumer
 for message in consumer:
     # Decode message từ binary thành chuỗi
@@ -30,20 +25,22 @@ for message in consumer:
     # In thông tin ra màn hình hoặc xử lý theo yêu cầu
     print(f"Riot Name: {riot_name}, Riot Tagline: {riot_tagline}")
 
-    url = f"https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{riot_name}/{riot_tagline}?api_key={api_key}"
+    url = f"https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-sid/{riot_name}/{riot_tagline}?api_key={api_key}"
     response = requests.get(url)
     if response.status_code == 200:
         summoner_data = response.json()
 
         # Lấy trường puuid
         puuid = summoner_data.get('puuid', '')
-        producer.send(f'puuid_{current_year}_{current_month}_{current_day}', f'{puuid}'.encode())
+        producer.send(f'puuid_{current_date}', f'{puuid}'.encode())
         producer.flush()
         print(f"Got {riot_name}, {riot_tagline}, {puuid}")
 
     else:
         print(f"Error with {riot_name}, {riot_tagline}: Status code {response.status_code}")
-
+        producer.send(f"riot_id_{current_date}", f"{riot_id}".encode())
+        print(f"Reproduced {riot_name}")
+        
 # Đừng quên close consumer khi không còn sử dụng
 consumer.close()
 producer.close()
